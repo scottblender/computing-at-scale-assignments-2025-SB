598431d0913cc03f1a46152f8b624d2a9e8c7ede
Scott Blender <scottblender20@gmail.com>
2025-01-13 19:56:03 -0500

    Added matrix vector product function.

---
+++ matrix_vector_product.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using std::vector;
+
+typedef vector<vector<int>> Matrix;
+typedef vector<int> Vector;
+
+Vector matrix_vector_product(const Matrix& m1, const Vector& v1){
+	int n = m1.size();
+	int m = m1[0].size();
+	Vector v2(n);
+
+	for (int i = 0; i < n; ++i){
+		int sum = 0;
+		for (int j = 0; j < m; ++j){
+			sum = sum + m1[i][j] * v1[j];
+		}
+		v2[i] = sum;
+	}
+	return c;
+}
